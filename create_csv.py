591497f8958ccfef55e3a3377eeecf83c0a85a07
dannyjf93 <dannygfavuzza@gmail.com>
2024-07-09 09:06:53 -0400

    Component to create extracted data folder and csv file with specific headers

---
+++ create_csv.py
@@ -0,0 +1,24 @@
+#import libraries needed
+import os
+import csv
+import requests
+
+#Create Exported_Data folder as well as .csv file within new folder
+def create_csv_file(csv_filename):
+    csv_directory = 'Exported_Data/Category_CSVs'
+    if not os.path.isdir(csv_directory):
+        os.mkdir(csv_directory)
+    with open('./Exported_Data/Category_CSVs/' + csv_filename, 'w', newline='', encoding-'utf-8') as csv_file:
+        book_csv = csv.writer(csv_file, delimiter=';')
+        book_csv.writerow([
+            'product_page_url',
+            'UPC',
+            'title',
+            'price_including_tax',
+            'price_excluding_tax',
+            'number_available',
+            'product_description',
+            'category',
+            'review-rating',
+            'image_url'
+        ])
