e0284c871a86cf611d236b49138d530355a0a4bd
dannyjf93 <dannygfavuzza@gmail.com>
2024-07-09 10:01:39 -0400

    updated to create new directory if it does not yet exist

--- create_csv.py
+++ create_csv.py
@@ -3,9 +3,17 @@ import os
 import csv
 import requests
 
+#Create directory
+export_directory = 'Exported_Data'
+#New venv would need to paste the appropriate parent directory below:
+parent_directory = 'C:/Open Classrooms Projects/Book_Scraping/Book_Scraper'
+path = os.path.join(parent_directory, export_directory)
+os.mkdir(path)
+
+
 #Create Exported_Data folder as well as .csv file within new folder
 def create_csv_file(csv_filename):
-    csv_directory = 'Exported_Data/Category_CSVs'
+    csv_directory = './Exported_Data/Category_CSVs'
     if not os.path.isdir(csv_directory):
         os.mkdir(csv_directory)
     with open('./Exported_Data/Category_CSVs/' + csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
