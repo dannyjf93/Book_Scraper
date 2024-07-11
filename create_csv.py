f59330c51b1fe85798f743e3815136485e8d0880
dannyjf93 <dannygfavuzza@gmail.com>
2024-07-11 08:47:38 -0400

    Component updated to dynamically create directory without need to manually input parent directory

--- create_csv.py
+++ create_csv.py
@@ -3,19 +3,16 @@ import os
 import csv
 import requests
 
-#Create directory
-export_directory = 'Exported_Data'
-#New venv would need to paste the appropriate parent directory below:
-parent_directory = 'C:/Open Classrooms Projects/Book_Scraping/Book_Scraper'
-path = os.path.join(parent_directory, export_directory)
-if not os.path.isdir(path):
-    os.mkdir(path)
+#Create directory if it does not currently exist
+export_directory = './Exported_Data'
+if not os.path.exists(export_directory):
+    os.mkdir(export_directory)
 
 
-#Create Exported_Data folder as well as .csv file within new folder
+#Create Category CSV folder as well as .csv file within new folder
 def create_csv_file(csv_filename):
     csv_directory = './Exported_Data/Category_CSVs'
-    if not os.path.isdir(csv_directory):
+    if not os.path.exists(csv_directory):
         os.mkdir(csv_directory)
     with open('./Exported_Data/Category_CSVs/' + csv_filename, 'w', newline='', encoding='utf-8') as csv_file:
         book_csv = csv.writer(csv_file, delimiter=';')
